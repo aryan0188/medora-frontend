d58d1221e4708093b47a7ce5aed812674bbdb18e
aryan0188 <159920009+aryan0188@users.noreply.github.com>
2025-04-09 19:46:31 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,23 @@
 import React from 'react';
-export default function Navbar() {
-  return <nav>Medora Navbar</nav>;
-}
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h2 className="logo">Medora</h2>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/search">Search</Link></li>
+        <li><Link to="/sideeffects">Side Effects</Link></li>
+        <li><Link to="/remedies">Remedies</Link></li>
+        <li><Link to="/medibot">MediBot</Link></li>
+        <li><Link to="/nearby">Nearby</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
