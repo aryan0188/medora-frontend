f685b27334d14d8f99aafb8d08aeb60a61c3b74a
aryan0188 <159920009+aryan0188@users.noreply.github.com>
2025-04-09 19:50:42 +0530

    Create Search.jsx

---
+++ src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Search = () => {
+  return (
+    <div className="page">
+      <h2>Search Medicine</h2>
+      <input type="text" placeholder="Enter medicine name..." />
+      <button>Search</button>
+    </div>
+  );
+};
+
+export default Search;
